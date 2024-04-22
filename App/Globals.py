4a9f5c88d85f4e0718a69f291b8f90606c79a664
Anusha Kalbande <anushakalbande17@gmail.com>
2024-04-22 14:15:47 -0400

    Removing Tokens

--- App/Globals.py
+++ App/Globals.py
@@ -10,12 +10,12 @@ output_audio_path = r"mixed_data\output_audio.wav"
 filepath = output_video_path + "\input_vid.mp4"
 
 # Hugging face token needed to downloaded the gated models
-hg_token = "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"
+hg_token = ""
 
 #Google API key - for Gemini based RAG
-GOOGLE_API_KEY = "AIzaSyDD1M74-7FmtrMAXglgTbMchf3lpedkr3g"
+GOOGLE_API_KEY = ""
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
 #Open AI key - for Open AI - Clip model
-OPENAI_API_TOKEN = "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"
+OPENAI_API_TOKEN = ""
 os.environ["OPENAI_API_KEY"] = OPENAI_API_TOKEN
